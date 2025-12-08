c8478890cb1b9c1e9810e6adab678f4bd0368135
Michael Canudas <michael@canudas.dev>
2025-12-07 21:29:57 -0500

    Update events.py

--- server/events.py
+++ server/events.py
@@ -1,5 +1,4 @@
 from state import logs
-from server import send_to_device
 
 
 events = {}
@@ -19,11 +18,13 @@ def trigger(name, *args, **kwargs):
 
 @on('magnetactive')
 def handle_magnet_active():
+    from server import send_to_device
     send_to_device('lights', 'red')
     logs.put('Sleigh magnet is now active')
 
 
 @on('magnetinactive')
 def handle_magnet_inactive():
+    from server import send_to_device
     send_to_device('lights', 'white')
     logs.put('Sleigh magnet is now inactive')
